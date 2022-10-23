43060c6e018639088e3b4e2c07e46dc511bee648
Igor Maksimov <st.igoriy@gmail.com>
2022-10-23 22:14:47 +0300

    23.10

--- modules.py
+++ modules.py
@@ -1,26 +1,5 @@
 import prop
 import numpy as np
-
-# class pump:
-#     def __init__(self,stream1,stream2,Pout,KPDpump,fluid,streams):
-#         self.stream1 = stream1
-#         self.stream2 = stream2
-#         self.streams = streams
-#         self.Pout = Pout
-#         self.KPDpump = KPDpump
-#         self.fluid = fluid
-        
-#     def calc(self):
-#         P1 = self.streams.at[self.stream1,"P"]
-#         H1 = self.streams.at[self.stream1,"H"]
-#         S1 = self.streams.at[self.stream1,"S"]
-#         G = self.streams.at[self.stream1,"G"]
-#         H2t = prop.p_s(self.Pout,S1,self.fluid)["H"]
-#         H2 = H1 + (H2t-H1)/self.KPDpump
-#         T2 = prop.h_p(H2,self.Pout,self.fluid)["T"]
-#         S2 = prop.h_p(H2,self.Pout,self.fluid)["S"]
-#         self.streams.loc[self.stream2, "T":"G"] = [T2, self.Pout, H2, S2, G]
-
         
 class pump:
     def calc(stream1,stream2,Pout,KPDpump,fluid,streams,blocks):
@@ -60,23 +39,15 @@ class regen:
             Q1 = streams.at[stream11,"Q"]
             Q2 = streams.at[stream21,"Q"]
         else:
-#             T22 = T11-dTmin
-#             H22 = prop.t_p(T22,P2,fluid)["H"]
-#             Q = G2*(H22-H21)
-#             H12 = H11 - (Q/G1)
-#             T12 = prop.h_p(H12,P1,fluid)["T"]
-            
             T12 = T21+dTmin
             H12 = prop.t_p(T12,P1,fluid)["H"]
             Q = G1*(H11-H12)
             H22 = H21 + (Q/G2) 
-
             T22 = prop.h_p(H22,P2,fluid)["T"]
             S22 = prop.h_p(H22,P2,fluid)["S"]
             S12 = prop.h_p(H12,P1,fluid)["S"]
             Q1 = prop.h_p(H12,P1,fluid)["Q"]
             Q2 = prop.h_p(H22,P2,fluid)["Q"]
-        
         streams.loc[stream12, "T":"Q"] = [T12,P1,H12,S12,G1,Q1]
         streams.loc[stream22, "T":"Q"] = [T22,P2,H22,S22,G2,Q2]
         blocks.loc['REGENERATOR','Q'] = Q
@@ -102,8 +73,6 @@ class heater:
             Q = G1*(H12-H11)
             H22 = H21 - (Q/G2)
             T22 = prop.h_p(H22,P2,fluid2)["T"]
-            
-
         S22 = prop.h_p(H22,P2,fluid2)["S"]
         S12 = prop.h_p(H12,P1,fluid1)["S"]
         Q1 = prop.h_p(H12,P1,fluid1)["Q"]
@@ -111,7 +80,6 @@ class heater:
         streams.loc[stream12, "T":"Q"] = [T12,P1,H12,S12,G1,Q1]
         streams.loc[stream22, "T":"Q"] = [T22,P2,H22,S22,G2,Q2]
         blocks.loc['HEATER','Q'] = Q
-
         
 class heater80:
     def calc(stream11,stream12,stream21,stream22,dTmin_heat,Toutmin,fluid1,fluid2,streams,blocks):
@@ -128,7 +96,6 @@ class heater80:
         T22 = T11-dTmin_heat
         H22 = prop.t_p(T22,P2,fluid2)["H"]
         G2 = Q/(H22-H21)
-
         S22 = prop.h_p(H22,P2,fluid2)["S"]
         S12 = prop.h_p(H12,P1,fluid1)["S"]
         Q1 = prop.h_p(H12,P1,fluid1)["Q"]
@@ -136,8 +103,7 @@ class heater80:
         streams.loc[stream12, "T":"Q"] = [T12,P1,H12,S12,G1,Q1]
         streams.loc[stream22, "T":"Q"] = [T22,P2,H22,S22,G2,Q2]
         blocks.loc['HEATER','Q'] = Q
-        
-        
+
 class turbine:
     def calc(stream1,stream2,Pout,KPDturb,fluid,streams,blocks):
         P1 = streams.at[stream1,"P"]
@@ -153,7 +119,6 @@ class turbine:
         N = G*(H1-H2)
         blocks.loc['TURBINE', 'N'] = N
 
-        
 class condenser:
     def calc(stream1,stream2, fluid,streams,blocks):
         Pcond = streams.at[stream1,"P"]
