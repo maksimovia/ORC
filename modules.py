609645c38ebdfc299a3db4cf1ccabeaeab85ceee
Igor Maksimov <st.igoriy@gmail.com>
2023-02-07 08:54:47 +0300

    07.02.23

--- modules.py
+++ modules.py
@@ -1,6 +1,82 @@
 import prop
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import root
+
+class heater:
+    def calc(stream11,stream12,stream21,stream22,Toutmin,minDTheater,fluid1,fluid2,streams,blocks):
+        H11 = streams.at[stream11,"H"]
+        H21 = streams.at[stream21,"H"]
+        P1 = streams.at[stream11,"P"]
+        P2 = streams.at[stream21,"P"]
+        G1 = streams.at[stream11,"G"]
+        G2 = streams.at[stream21,"G"]
+        T12 = Toutmin
+        H12 = prop.t_p(T12,P1,fluid1)["H"]
+        s = 50
+        step = (H11-H12)/s
         
+        def G2sved(G2):
+            t1  = np.zeros(s+1)
+            t2  = np.zeros(s+1)
+            Q   = np.zeros(s+1)
+            h11 = H11
+            h21 = H21
+            for i in range(s+1):
+                t1[i] = prop.h_p(h11,P1,fluid1)["T"]
+                if i < s:
+                    h12 = h11-step
+                    dQ = G1*(h11-h12)
+                    h11 = h12
+                    Q[i+1] = Q[i]+dQ
+            for i in range(s+1):
+                t2[s-i] = prop.h_p(h21,P2,fluid2)["T"]
+                if i < s:
+                    h22 = h21+(Q[s-i]-Q[s-i-1])/G2
+                    h21 = h22
+            T12 = t1[s]
+            T22 = t2[0]
+            H12 = h11
+            H22 = h21
+            DT=t1-t2
+            minDT=round(min(DT),5)
+            return minDT-minDTheater
+        sol = root(G2sved, G2)
+        G2 = float(sol.x)
+        
+        t1  = np.zeros(s+1)
+        t2  = np.zeros(s+1)
+        Q   = np.zeros(s+1)
+        h11 = H11
+        h21 = H21
+        for i in range(s+1):
+            t1[i] = prop.h_p(h11,P1,fluid1)["T"]
+            if i < s:
+                h12 = h11-step
+                dQ = G1*(h11-h12)
+                h11 = h12
+                Q[i+1] = Q[i]+dQ
+        for i in range(s+1):
+            t2[s-i] = prop.h_p(h21,P2,fluid2)["T"]
+            if i < s:
+                h22 = h21+(Q[s-i]-Q[s-i-1])/G2
+                h21 = h22
+        T12 = t1[s]
+        T22 = t2[0]
+        H12 = h11
+        H22 = h21
+        DT=t1-t2
+        minDT=round(min(DT),5)
+        S12 = prop.h_p(H12,P1,fluid1)["S"]
+        S22 = prop.h_p(H22,P2,fluid2)["S"]
+        Q12 = prop.h_p(H12,P1,fluid1)["Q"]
+        Q22 = prop.h_p(H22,P2,fluid2)["Q"]
+        streams.loc[stream12, "T":"Q"] = [T12,P1,H12,S12,G1,Q12]
+        streams.loc[stream22, "T":"Q"] = [T22,P2,H22,S22,G2,Q22]
+        blocks.loc['HEATER','Q'] = Q[s]
+#         plt.plot(Q,t1,Q,t2)
+#         plt.show()
+
 class pump:
     def calc(stream1,stream2,Pout,KPDpump,fluid,streams,blocks):
         P1 = streams.at[stream1,"P"]
@@ -18,91 +94,65 @@ class pump:
         
 class regen:
     def calc(stream11,stream12,stream21,stream22,dTmin,fluid,streams,blocks):
-        T11 = streams.at[stream11,"T"]
         H11 = streams.at[stream11,"H"]
-        T21 = streams.at[stream21,"T"]
         H21 = streams.at[stream21,"H"]
+        T11 = streams.at[stream11,"T"]
+        T21 = streams.at[stream21,"T"]
+        S11 = streams.at[stream11,"S"]
+        S21 = streams.at[stream21,"S"]
         P1 = streams.at[stream11,"P"]
         P2 = streams.at[stream21,"P"]
         G1 = streams.at[stream11,"G"]
         G2 = streams.at[stream21,"G"]
-        S21 = streams.at[stream21,"S"]
-        S11 = streams.at[stream11,"S"]
-        if np.isnan(T11):
+        s = 50
+        t1   = np.zeros(s+1)
+        t2   = np.zeros(s+1)
+        Q    = np.zeros(s+1)
+        if np.isnan(H11):
             T22 = T21
             T12 = T11
             H12 = H11
             H22 = H21
             S22 = S21
             S12 = S11
-            Q = 0
-            Q1 = streams.at[stream11,"Q"]
-            Q2 = streams.at[stream21,"Q"]
+            Q[s] = 0
+            Q12 = streams.at[stream11,"Q"]
+            Q22 = streams.at[stream21,"Q"]
         else:
             T12 = T21+dTmin
             H12 = prop.t_p(T12,P1,fluid)["H"]
-            Q = G1*(H11-H12)
-            H22 = H21 + (Q/G2) 
-            T22 = prop.h_p(H22,P2,fluid)["T"]
+            step = (H11-H12)/s
+            h11 = H11
+            h21 = H21
+            for i in range(s+1):
+                t1[i] = prop.h_p(h11,P1,fluid)["T"]
+                if i < s:
+                    h12 = h11-step
+                    dQ = G1*(h11-h12)
+                    h11 = h12
+                    Q[i+1] = Q[i]+dQ
+            for i in range(s+1):
+                t2[s-i] = prop.h_p(h21,P2,fluid)["T"]
+                if i < s:
+                    h22 = h21+(Q[s-i]-Q[s-i-1])/G2
+                    h21 = h22
+            T12 = t1[s]
+            T22 = t2[0]
+            H12 = h11
+            H22 = h21
+            DT=t1-t2
+            minDT=round(min(DT),5)
+            if minDT < dTmin:
+                print('!REGEN:minDT<dTmin=',minDT)
             S22 = prop.h_p(H22,P2,fluid)["S"]
             S12 = prop.h_p(H12,P1,fluid)["S"]
-            Q1 = prop.h_p(H12,P1,fluid)["Q"]
-            Q2 = prop.h_p(H22,P2,fluid)["Q"]
-        streams.loc[stream12, "T":"Q"] = [T12,P1,H12,S12,G1,Q1]
-        streams.loc[stream22, "T":"Q"] = [T22,P2,H22,S22,G2,Q2]
-        blocks.loc['REGENERATOR','Q'] = Q
-        
-class heater:
-    def calc(stream11,stream12,stream21,stream22,dTmin_heat,Toutmin,fluid1,fluid2,streams,blocks):
-        T11 = streams.at[stream11,"T"]
-        H11 = streams.at[stream11,"H"]
-        T21 = streams.at[stream21,"T"]
-        H21 = streams.at[stream21,"H"]
-        P1 = streams.at[stream11,"P"]
-        P2 = streams.at[stream21,"P"]
-        G1 = streams.at[stream11,"G"]
-        G2 = streams.at[stream21,"G"]
-        T22 = T11-dTmin_heat
-        H22 = prop.t_p(T22,P2,fluid2)["H"]
-        Q = G2*(H22-H21)
-        H12 = H11 - (Q/G1)
-        T12 = prop.h_p(H12,P1,fluid1)["T"]
-        if T12 < Toutmin:
-            T12 = Toutmin
-            H12 = prop.t_p(T12,P1,fluid1)["H"]
-            Q = G1*(H12-H11)
-            H22 = H21 - (Q/G2)
-            T22 = prop.h_p(H22,P2,fluid2)["T"]
-        S22 = prop.h_p(H22,P2,fluid2)["S"]
-        S12 = prop.h_p(H12,P1,fluid1)["S"]
-        Q1 = prop.h_p(H12,P1,fluid1)["Q"]
-        Q2 = prop.h_p(H22,P2,fluid2)["Q"]
-        streams.loc[stream12, "T":"Q"] = [T12,P1,H12,S12,G1,Q1]
-        streams.loc[stream22, "T":"Q"] = [T22,P2,H22,S22,G2,Q2]
-        blocks.loc['HEATER','Q'] = Q
-        
-class heater80:
-    def calc(stream11,stream12,stream21,stream22,dTmin_heat,Toutmin,fluid1,fluid2,streams,blocks):
-        T11 = streams.at[stream11,"T"]
-        H11 = streams.at[stream11,"H"]
-        T21 = streams.at[stream21,"T"]
-        H21 = streams.at[stream21,"H"]
-        P1 = streams.at[stream11,"P"]
-        P2 = streams.at[stream21,"P"]
-        G1 = streams.at[stream11,"G"]
-        T12 = Toutmin
-        H12 = prop.t_p(T12,P1,fluid1)["H"]
-        Q = G1*(H11-H12)
-        T22 = T11-dTmin_heat
-        H22 = prop.t_p(T22,P2,fluid2)["H"]
-        G2 = Q/(H22-H21)
-        S22 = prop.h_p(H22,P2,fluid2)["S"]
-        S12 = prop.h_p(H12,P1,fluid1)["S"]
-        Q1 = prop.h_p(H12,P1,fluid1)["Q"]
-        Q2 = prop.h_p(H22,P2,fluid2)["Q"]
-        streams.loc[stream12, "T":"Q"] = [T12,P1,H12,S12,G1,Q1]
-        streams.loc[stream22, "T":"Q"] = [T22,P2,H22,S22,G2,Q2]
-        blocks.loc['HEATER','Q'] = Q
+            Q12 = prop.h_p(H12,P1,fluid)["Q"]
+            Q22 = prop.h_p(H22,P2,fluid)["Q"]
+#             plt.plot(Q,t1,Q,t2)
+#             plt.show()
+        streams.loc[stream12, "T":"Q"] = [T12,P1,H12,S12,G1,Q12]
+        streams.loc[stream22, "T":"Q"] = [T22,P2,H22,S22,G2,Q22]
+        blocks.loc['REGENERATOR','Q'] = Q[s]
 
 class turbine:
     def calc(stream1,stream2,Pout,KPDturb,fluid,streams,blocks):
