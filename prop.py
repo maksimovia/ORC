23cd28aa60bbf1d290a1304fe74490670aef6464
Igor Maksimov <st.igoriy@gmail.com>
2023-04-23 22:26:10 +0300

    23.04

--- prop.py
+++ prop.py
@@ -1,55 +1,66 @@
-from CoolProp.CoolProp import PropsSI
-import json, CoolProp.CoolProp as CP
-CP.set_config_string(CP.ALTERNATIVE_REFPROP_PATH, 'C:/Program Files (x86)/REFPROP')
+from ctREFPROP.ctREFPROP import REFPROPFunctionLibrary
+RP = REFPROPFunctionLibrary('C:/Program Files (x86)/REFPROP')
+
+iUnits = 21
+iMass = 2
+iFlag = 0
 
 def t_p(T,P,fluid):
-    H = PropsSI('H','T', T+273.15, 'P', P*10**6, fluid)/1000
-    S = PropsSI('S','T', T+273.15, 'P', P*10**6, fluid)/1000
-    Q = PropsSI('Q','T', T+273.15, 'P', P*10**6, fluid)
-    if Q>=1:
-        Q=1
-    elif Q<=0:
+    comp=[]
+    for i in range(1, len(fluid.split(";")),2): comp.append(float(fluid.split(";")[i]))
+    res = RP.REFPROPdll(fluid, 'TP', 'T;P;H;S', iUnits, iMass, iFlag, T+273.15, P*10**6, comp)
+    if res.q<=0:
         Q=0
-    return {'T':T, 'P':P, 'H':H, 'S':S, 'Q':Q}
+    elif res.q>=1:
+        Q=1
+    else:
+        print('t_p: двухфазная среда')
+    return {'T':res.Output[0]-273.15, 'P':res.Output[1]/10**6, 'H':res.Output[2]/1000, 'S':res.Output[3]/1000, 'Q':Q}
 
 def h_p(H,P,fluid):
-    T = PropsSI('T', 'H', H*1000, 'P', P*10**6, fluid)-273.15
-    S = PropsSI('S', 'H', H*1000, 'P', P*10**6, fluid)/1000
-    Q = PropsSI('Q', 'H', H*1000, 'P', P*10**6, fluid)
-    if Q>=1:
-        Q=1
-    elif Q<=0:
+    comp=[]
+    for i in range(1, len(fluid.split(";")),2): comp.append(float(fluid.split(";")[i]))
+    res = RP.REFPROPdll(fluid, 'HP', 'T;P;H;S;QMASS', iUnits, iMass, iFlag, H*1000, P*10**6, comp)
+    if res.q<=0:
         Q=0
-    return {'T':T, 'P':P, 'H':H, 'S':S, 'Q':Q}
+    elif res.q>=1:
+        Q=1
+    else:
+        Q = res.Output[4]
+    return {'T':res.Output[0]-273.15, 'P':res.Output[1]/10**6, 'H':res.Output[2]/1000, 'S':res.Output[3]/1000, 'Q':Q}
 
 def p_q(P,Q,fluid):
-    T = PropsSI('T', 'P', P*10**6,'Q', Q, fluid)-273.15
-    H = PropsSI('H', 'P', P*10**6,'Q', Q, fluid)/1000
-    S = PropsSI('S', 'P', P*10**6,'Q', Q, fluid)/1000
-    return {'T':T, 'P':P, 'H':H, 'S':S, 'Q':Q}
+    comp=[]
+    for i in range(1, len(fluid.split(";")),2): comp.append(float(fluid.split(";")[i]))
+    res = RP.REFPROPdll(fluid, 'PQ', 'T;P;H;S', iUnits, iMass, iFlag, P*10**6, Q, comp)
+    return {'T':res.Output[0]-273.15, 'P':res.Output[1]/10**6, 'H':res.Output[2]/1000, 'S':res.Output[3]/1000, 'Q':Q}
 
 def t_q(T,Q,fluid):
-    P = PropsSI('P', 'T', T+273.15, 'Q', Q, fluid)/(10**6)
-    H = PropsSI('H', 'T', T+273.15, 'Q', Q, fluid)/1000
-    S = PropsSI('S', 'P', P*10**6,  'Q', Q, fluid)/1000
-    return {'T':T, 'P':P, 'H':H, 'S':S, 'Q':Q}
+    comp=[]
+    for i in range(1, len(fluid.split(";")),2): comp.append(float(fluid.split(";")[i]))
+    res = RP.REFPROPdll(fluid, 'TQ', 'T;P;H;S', iUnits, iMass, iFlag, T+273.15, Q, comp)
+    return {'T':res.Output[0]-273.15, 'P':res.Output[1]/10**6, 'H':res.Output[2]/1000, 'S':res.Output[3]/1000, 'Q':Q}
 
 def p_s(P,S,fluid):
-    T = PropsSI('T', 'P', P*10**6, 'S', S*1000, fluid)-273.15
-    H = PropsSI('H', 'P', P*10**6, 'S', S*1000, fluid)/1000
-    Q = PropsSI('Q', 'P', P*10**6, 'S', S*1000, fluid)
-    if Q>=1:
-        Q=1
-    elif Q<=0:
+    comp=[]
+    for i in range(1, len(fluid.split(";")),2): comp.append(float(fluid.split(";")[i]))
+    res = RP.REFPROPdll(fluid, 'PS', 'T;P;H;S;QMASS', iUnits, iMass, iFlag, P*10**6, S*1000, comp)
+    if res.q<=0:
         Q=0
-    return {'T':T, 'P':P, 'H':H, 'S':S, 'Q':Q}
+    elif res.q>=1:
+        Q=1
+    else:
+        Q = res.Output[4]
+    return {'T':res.Output[0]-273.15, 'P':res.Output[1]/10**6, 'H':res.Output[2]/1000, 'S':res.Output[3]/1000, 'Q':Q}
 
 def t_s(T,S,fluid):
-    P = PropsSI('P', 'T', T+273.15, 'S', S*1000, fluid)/(10**6)
-    H = PropsSI('H', 'T', T+273.15, 'S', S*1000, fluid)/1000
-    Q = PropsSI('Q', 'T', T+273.15, 'S', S*1000, fluid)
-    if Q>=1:
-        Q=1
-    elif Q<=0:
+    comp=[]
+    for i in range(1, len(fluid.split(";")),2): comp.append(float(fluid.split(";")[i]))
+    res = RP.REFPROPdll(fluid, 'TS', 'T;P;H;S;QMASS', iUnits, iMass, iFlag, T+273.15, S*1000, comp)
+    if res.q<=0:
         Q=0
-    return {'T':T, 'P':P, 'H':H, 'S':S, 'Q':Q}
+    elif res.q>=1:
+        Q=1
+    else:
+        Q = res.Output[4]
+    return {'T':res.Output[0]-273.15, 'P':res.Output[1]/10**6, 'H':res.Output[2]/1000, 'S':res.Output[3]/1000, 'Q':Q}
