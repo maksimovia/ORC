249f72da310ac4e32a0861805ee7bdfc405408ee
Igor Maksimov <st.igoriy@gmail.com>
2023-07-05 18:47:30 +0300

    05.07

--- main.py
+++ main.py
@@ -4,21 +4,30 @@ open_db()
 close_db()
 open_db()
 
-
+# Входной поток газа
 G_gas = 509
 T_gas = 183.6
 P_gas = 0.1
 T_gas_out = 80
 X_gas = "N2;0.78;O2;0.1;CO2;0.02;H2O;0.1"
-
-
-# Заполнение строки IN-HEAT
 write_stream('IN-HEAT',T_gas,P_gas,prop.t_p(T_gas,P_gas,X_gas)["H"],prop.t_p(T_gas,P_gas,X_gas)["S"],prop.t_p(T_gas,P_gas,X_gas)["Q"],G_gas,X_gas)
-write_stream('PUMP-HEAT',50,0.25,prop.t_p(50,0.25,'R236ea')["H"],1,1,300,'R236ea')
 
 
+# Насос
+p_pump = 3.4
+kpd_pump = 0.9
+
+# Поток после конденсатора
+T_cond = 25
+P_cond = 0.24
+X_cond = 'R236ea'
+G_cond = 300
+write_stream('COND-PUMP',T_cond,P_cond,prop.t_p(T_cond,P_cond,X_cond)["H"],prop.t_p(T_cond,P_cond,X_cond)["S"],prop.t_p(T_cond,P_cond,X_cond)["Q"],G_cond,X_cond)
 
 import modules1
+pump = modules1.PUMP('COND-PUMP','PUMP-HEAT', p_pump, kpd_pump)
+pump.calc()
+
 heater = modules1.HEATER('IN-HEAT','HEAT-OUT','PUMP-HEAT','HEAT-TURB', 5, 80)
 heater.calc()
 
--- modules1.py
+++ modules1.py
@@ -3,17 +3,18 @@ from sqlite import read_stream, write_stream
 import numpy as np
 
 tolerance = 10 ** -5
-h_steps = 5
+h_steps = 3
 
 
 def root(func, a, b, root_tol):
-    while func(b)-func(a) > root_tol:
+    while func(a)-func(b) > root_tol:
         x = a + (b-a)/2
         if func(a)*func(x) < 0:
             b = x
         else:
             a = x
     return x
+
 
 
 class HEATER:
@@ -31,8 +32,6 @@ class HEATER:
         H11 = read_stream(self.stream11)['H']
         G1 = read_stream(self.stream11)['G']
         fluid1 = read_stream(self.stream11)['X']
-
-        print(read_stream(self.stream21)['T'])
         T21 = read_stream(self.stream21)['T']
         P21 = read_stream(self.stream21)['P']
         H21 = read_stream(self.stream21)['H']
@@ -44,73 +43,75 @@ class HEATER:
         S12 = prop.t_p(T12, P11, fluid1)["S"]
         Q12 = prop.t_p(T12, P11, fluid1)["Q"]
 
-        ##########################################
-
         step = (H11 - H12) / h_steps
-
-        def G2_root(G2):
+        def G2_func(G2):
             t1 = np.zeros(h_steps + 1)
             t2 = np.zeros(h_steps + 1)
             Q = np.zeros(h_steps + 1)
             h11 = H11
             h21 = H21
-            print(t1,Q)
-            for i in t1:
+            for i in range(h_steps + 1):
                 t1[i] = prop.h_p(h11, P11, fluid1)["T"]
                 if i < h_steps:
                     h12 = h11 - step
                     dQ = G1 * (h11 - h12)
                     h11 = h12
                     Q[i + 1] = Q[i] + dQ
-            for i in t1:
+            for i in range(h_steps + 1):
                 t2[h_steps - i] = prop.h_p(h21, P21, fluid2)["T"]
                 if i < h_steps:
                     h22 = h21 + (Q[h_steps - i] - Q[h_steps - i - 1]) / G2
                     h21 = h22
-
             DT = t1 - t2
-            minDT = min(DT)
-            return self.dT - minDT
-
-        ###########################################
-
-        T22 = 1
-        H22 = 1
-        S22  =1
-        Q22 = 1
+            min_dt = min(DT)
+            return self.dT - min_dt
+        G2 = root(G2_func, 1, 10000, tolerance)
+        t1 = np.zeros(h_steps + 1)
+        t2 = np.zeros(h_steps + 1)
+        Q = np.zeros(h_steps + 1)
+        h11 = H11
+        h21 = H21
+        for i in range(h_steps + 1):
+            t1[i] = prop.h_p(h11, P11, fluid1)["T"]
+            if i < h_steps:
+                h12 = h11 - step
+                dQ = G1 * (h11 - h12)
+                h11 = h12
+                Q[i + 1] = Q[i] + dQ
+        for i in range(h_steps + 1):
+            t2[h_steps - i] = prop.h_p(h21, P21, fluid2)["T"]
+            if i < h_steps:
+                h22 = h21 + (Q[h_steps - i] - Q[h_steps - i - 1]) / G2
+                h21 = h22
+        DT = t1 - t2
+        min_dt = min(DT)
+        T22 = t2[0]
+        H22 = prop.t_p(T22,P21,fluid2)["H"]
+        S22 = prop.t_p(T22,P21,fluid2)["S"]
+        Q22 = prop.t_p(T22,P21,fluid2)["Q"]
         write_stream(self.stream12,T12,P11,H12,S12,Q12,G1,fluid1)
         write_stream(self.stream22,T22,P21,H22,S22,Q22,G2,fluid2)
+        # write_block('HEATER',Q[-1])
         pass
 
+class PUMP:
+    def __init__(self, stream1, stream2, p_out, kpd_pump):
+        self.stream1 = stream1
+        self.stream2 = stream2
+        self.p_out = p_out
+        self.kpd_pump = kpd_pump
 
+    def calc(self):
+        P1 = read_stream(self.stream1)['P']
+        H1 = read_stream(self.stream1)['H']
+        S1 = read_stream(self.stream1)['S']
+        G = read_stream(self.stream1)['G']
+        fluid = read_stream(self.stream1)['X']
+        H2t = prop.p_s(self.p_out, S1, fluid)["H"]
+        H2 = H1 + (H2t - H1) / self.kpd_pump
+        T2 = prop.h_p(H2, self.p_out, fluid)["T"]
+        S2 = prop.h_p(H2, self.p_out, fluid)["S"]
+        Q2 = prop.h_p(H2, self.p_out, fluid)["Q"]
+        N = G * (H2 - H1)
 
-    def TQ(self):
-        pass
-
-
-# class REGEN:
-#     def __init__(self):
-#     def calc(self):
-#     def TQ(self):
-#         pass
-#
-#
-# class CONDENSER:
-#     def __init__(self):
-#     def calc(self):
-#     def TQ(self):
-#         pass
-#
-#
-# class PUMP:
-#     def __init__(self):
-#     def calc(self):
-#     def HS(self):
-#         pass
-#
-#
-# class TURBINE:
-#     def __init__(self):
-#     def calc(self):
-#     def HS(self):
-#         pass
+        write_stream(self.stream2,T2,self.p_out,H2,S2,Q2,G,fluid)
