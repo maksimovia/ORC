552d4e946a447899aeb4dbffff48fd92cb49cfbb
Igor Maksimov <st.igoriy@gmail.com>
2024-05-31 13:27:01 +0300

    31.05

--- regen_multiprocess.py
+++ regen_multiprocess.py
@@ -2,11 +2,11 @@ import numpy as np
 import pandas as pd
 import prop
 from modules import PUMP, HEAT, TURB, COND, REG
+from multiprocessing import Process, active_children
+from time import sleep
 
-streams_list = ["IN-HEAT", "HEAT-OUT", "PUMP-REG","REG-HEAT", "HEAT-TURB", "TURB-REG", "REG-COND", "COND-PUMP",]
-block_list = ["PUMP", "TURB", "HEAT", "COND","REG",]
-streams = pd.DataFrame(index=streams_list, columns=["T", "P", "H", "S", "Q", "G", "X"])
-blocks = pd.DataFrame(index=block_list, columns=["N", "Q", "dT", "T1i", "T2i", "Qi"])
+global data
+data = open("res.txt", "a")
 
 KPDpump = 0.85
 KPDturb = 0.85
@@ -32,10 +32,15 @@ Xgas = ("N2;0.756199238;"
 Tgas_out = 80
 
 def cycle_calc(Tgas, Xorc, Ppump, dTreg, dPreg):
+    streams_list = ["IN-HEAT", "HEAT-OUT", "PUMP-REG", "REG-HEAT", "HEAT-TURB", "TURB-REG", "REG-COND", "COND-PUMP", ]
+    block_list = ["PUMP", "TURB", "HEAT", "COND", "REG", ]
+    streams = pd.DataFrame(index=streams_list, columns=["T", "P", "H", "S", "Q", "G", "X"])
+    blocks = pd.DataFrame(index=block_list, columns=["N", "Q", "dT", "T1i", "T2i", "Qi"])
+
     streams.loc['IN-HEAT'] = [Tgas, Pgas, prop.t_p(Tgas, Pgas, Xgas)['H'], prop.t_p(Tgas, Pgas, Xgas)['S'], prop.t_p(Tgas, Pgas, Xgas)['Q'], Ggas, Xgas]
     streams.loc['COND-PUMP'] = [Tcond, prop.t_q(Tcond, 0, Xorc)["P"], prop.t_q(Tcond, 0, Xorc)["H"], prop.t_q(Tcond, 0, Xorc)["S"], 0, 1000, Xorc]
 
-    for i in range(9999):
+    for i in range(100):
         PUMP('COND-PUMP', 'PUMP-REG', Ppump, KPDpump, streams, blocks).calc()
         if i == 0:
             streams.loc['REG-HEAT'] = streams.loc['PUMP-REG']
@@ -46,12 +51,21 @@ def cycle_calc(Tgas, Xorc, Ppump, dTreg, dPreg):
         REG('TURB-REG', 'REG-COND', 'PUMP-REG', 'REG-HEAT', dTreg, hsteps, dPreg, dQreg, Tgas_out, dTheat, streams, blocks).calc()
         COND('REG-COND', 'COND-PUMP', streams, blocks).calc()
         balance = abs(blocks.loc['HEAT', "Q"]*dQheat + blocks.loc['PUMP', "N"] - blocks.loc['COND', "Q"] - blocks.loc['TURB', "N"] - (1-dQreg)*blocks.loc['REG', 'Q']) / blocks.loc['HEAT', "Q"]
-        print(balance)
-        if balance < 10**-6:
+        if balance < 10**-5:
             break
     Nnet = blocks.loc['TURB', "N"]*KPDm*KPDe - blocks.loc['PUMP', "N"]/KPDm/KPDe
     KPDnet = Nnet / blocks.loc['HEAT', "Q"]
     print(Tgas, Xorc, round(Ppump,5), round(Nnet,5), round(KPDnet,5), round(blocks.loc['HEAT', "dT"],5), streams.loc['HEAT-TURB', "Q"], streams.loc['TURB-REG', "Q"], balance)
+    data.write( '\n'+ str(Tgas)
+               +'\t'+ str(Xorc)
+               +'\t'+ str(Ppump)
+               +'\t'+ str(Nnet)
+               +'\t'+ str(KPDnet)
+               +'\t'+ str(blocks.loc['REG', "dT"])
+               +'\t'+ str(blocks.loc['HEAT', "dT"])
+               +'\t'+ str(streams.loc['HEAT-TURB', "Q"])
+               +'\t'+ str(streams.loc['TURB-REG', "Q"])
+               +'\t'+ str(balance))
 
 Tgas = 180
 Xorc = "R236ea"
@@ -59,10 +73,10 @@ Ppump = 4.1
 dTreg = 5
 dPreg = 10 * 10**-3
 
-cycle_calc(Tgas, Xorc, Ppump, dTreg, dPreg)
-
-# for Ppump in np.arange(3, 6, 0.1):
-#     cycle_calc(Tgas, Xorc, Ppump, dTreg, dPreg)
-
-print(streams.loc[:, "T":"G"])
-print(blocks)
+if __name__ == '__main__':
+    for Ppump in np.linspace(3,6,50):
+        calculating = Process(target=cycle_calc, args=(Tgas, Xorc, Ppump, dTreg, dPreg))
+        calculating.start()
+        while len(active_children()) > 59:
+            sleep(2)
+    data.close()
